df11d5b1dab50ba9a4e64b5c8bb330698484b50f
Mo DeJong <mo@modejong.com>
2019-01-23 11:40:43 -0800

    add target CoreVideoDecodeiOS that will capture output of CoreVideo 196 default gamma decoding to a PNG

---
+++ CoreVideoDecodeiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreVideoDecodeiOS
+//
+//  Created by Mo DeJong on 1/23/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
